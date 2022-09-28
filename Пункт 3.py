e3d41824b9a57bc193ab304711f16709dbfc3992
DariaStupnikova <113526180+DariaStupnikova@users.noreply.github.com>
2022-09-28 21:23:18 +0300

    Update Пункт 3.py

--- Пункт 3.py
+++ Пункт 3.py
@@ -1,12 +1,8 @@
 import csv
-count = 0
 with open("books.csv", encoding='cp1251') as r_file:
     file_reader = csv.DictReader(r_file, delimiter=";")
     autor = input() #Ввод ФИО автора
     for lst in file_reader: #Пробегаемся по массиву, ищем совпадения между запросом и файлом
         if autor==lst["Автор (ФИО)"]:
-            count += 1
-            if count <= 3:
+            if lst["Дата поступления"][6:10] == '2014' or lst["Дата поступления"][6:10] == '2016' or lst["Дата поступления"][6:10] == '2017':
                 print(lst["Название"])
-            else: break
-    
